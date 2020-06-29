9a0ba033446041e8ad7886fe9fa55fba05fa54a0
Sandeep <sk9958814616@gmail.com>
2020-06-29 14:17:03 +0530

    Update settings.py

--- Blogger/settings.py
+++ Blogger/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'f3#1d!k2_+=g^n&x5wcj_4$&l%pulcy1s%t34zfcf44=al$9*w'
+SECRET_KEY = 'your-django-project-secret-key-here'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
